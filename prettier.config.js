8508376135d19abfb6683700ff2626920b7b1448
Julia Zolotarev <jzolotarev@gmail.com>
2022-08-09 19:37:02 -0400

    add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  proseWrap: 'always',
+  endOfLine: 'lf',
+  pluginSearchDirs: ['/'],
+  importOrder: ['^react', '<THIRD_PARTY_MODULES>', '^[~/]', '^[./]'],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: false,
+};
